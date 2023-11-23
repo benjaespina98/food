17422760c0c1a70cd2f023bbe275cbdb270d925d
benjaespina98 <83318957+benjaespina98@users.noreply.github.com>
2023-11-23 04:53:44 -0300

    Nuevas categorías

--- foodapp/src/app/tab1/tab1.page.ts
+++ foodapp/src/app/tab1/tab1.page.ts
@@ -1,65 +1,20 @@
 import { Component } from '@angular/core';
 
-export interface Food {
-  name: string;
-  portion: string;
-}
-
-export interface Category {
-  name: string;
-  image?: string;
-  foods: Food[];
-  expanded: boolean;
-}
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  categories: Category[] = [
-    {
-      name: 'Almidones',
-      image: 'assets/bread48x48.png',
-      foods: [
-        { name: 'Pan árabe', portion: '1 unidad grande' },
-        // Resto de los alimentos de la categoría Almidones
-      ],
-      expanded: false
-    },
-    {
-      name: 'Panes',
-      image: 'assets/bread48x48.png',
-      foods: [
-        { name: 'Pan de hamburguesa', portion: '1 unidad' },
-        // Resto de los alimentos de la categoría Panes
-      ],
-      expanded: false
-    },
-    {
-      name: 'Frutas',
-      image: 'assets/redapple256x256.png',
-      foods: [
-        { name: 'Pan de hamburguesa', portion: '1 unidad' },
-        // Resto de los alimentos de la categoría Panes
-      ],
-      expanded: false
-    },
-    {
-      name: 'Frutas',
-      image: 'assets/redapple512x512.png',
-      foods: [
-        { name: 'Pan de hamburguesa', portion: '1 unidad' },
-        // Resto de los alimentos de la categoría Panes
-      ],
-      expanded: false
-    },
-  ];
+  days: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  selectedDay: string | null = null;
+  meals: string[] = ['Desayuno', 'Almuerzo', 'Merienda', 'Cena'];
 
-  toggleCategory(category: Category) {
-    category.expanded = !category.expanded;
+  showMeals(day: string) {
+    if (this.selectedDay === day) {
+      this.selectedDay = null;
+    } else {
+      this.selectedDay = day;
+    }
   }
-
-  constructor() { }
 }
